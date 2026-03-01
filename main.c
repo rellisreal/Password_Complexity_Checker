f5412a8b493902ac51581e958df550d803f2c89e
rellisreal <gendarkdragon@gmail.com>
2026-03-01 20:04:32 +0800

    removed the ugly if-else checks when confirming the output

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include "logger/logger.h"
+#include <iso646.h>
+#include <stdio.h>
 
 int main() {
     const char capitalValues[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const char numericValues[] = "1234567890";
     const char symbolicValues[] = "!@#$%^&*()_+-=;'[],.<>/:";
+    int results[3] = {0,0,0};
+    int i; 
     char buffer[1000];
     char *password;
-    int i; 
-    int hasCaps = 0;
-    int hasNumeric = 0; 
-    int hasSymbol= 0; 
-    
+
     log_message("Starting Application");
     printf("\nRequirements: 8+ Charachters\n-Numeric Charachter \n-Capital Chara \nPlease insert a password: ");
     scanf("%999s", buffer);
@@ -28,29 +28,28 @@ int main() {
     strcpy(password,buffer);
 
     for (i = 0; i < strlen(password); i++) {
-        if (strchr(capitalValues, password[i])) {hasCaps=1;}
-        if (strchr(numericValues, password[i])) {hasNumeric=1;}
-        if (strchr(symbolicValues, password[i])) {hasSymbol=1;}
+        if (strchr(capitalValues, password[i])) {results[0]=1;}
+        if (strchr(numericValues, password[i])) {results[1]=1;}
+        if (strchr(symbolicValues, password[i])) {results[2]=1;}
     }
     free(password);
-    if (hasCaps==1 && hasNumeric==1 && hasSymbol==1) {
-        printf("Password Complexity Test Passed\n");
-        return 0; 
+    if (complexityChecker(results) == 1) {return 1;};
+    return 0;
+}
+
+int complexityChecker(int results[]) {
+    int i; 
+    log_message("Checking Complexity");
+    for (i = 0; i < 3; i++) {
+        if (results[i] == 0)
+        {
+            printf("\n the number is %d \n", i);
+            if (i == 0) {printf("Please add a capital charachter\n");};
+            if (i == 1) {printf("Please add a numeric charachter\n");};
+            if (i == 2) {printf("Please add a symbol\n");};
+            return 1;
+        };
     }
-    else if (hasCaps==0) {
-        printf("Please add a capital charachter\n");
-        return 1;
-    }
-    else if (hasNumeric==0) {
-        printf("Please add a Numeric charachter\n");
-        return 1;
-    }
-    else if (hasSymbol==0) {
-        printf("Please add a Symbolic charachter\n");
-        return 1;
-    }
-    else {
-        printf("Password Complexity Test Failed\n");
-        return 1; 
-    }
+    printf("Password is secure\n");   
+    return 0; 
 }
